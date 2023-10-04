f59fa07e8c2ea3b3995517d6961924f9fe9859a3
laurinmuller <laurin.mueller1@web.de>
2023-10-04 19:57:51 +0200

    adding dist

---
+++ src/components/profile/QuestionFilter.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "QuestionFilter"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/profile/StatusBar.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "StatusBar"
+}
+</script>
+
+<style scoped>
+
+</style>
